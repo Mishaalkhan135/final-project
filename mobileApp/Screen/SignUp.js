425a1f9dc62b4480f56ba831f94648751b6aa018
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2021-12-28 17:37:12 +0500

    project

---
+++ mobileApp/Screen/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>Sign Up Screen</Text>
+    </View>
+  );
+};
+export default SignUp;
